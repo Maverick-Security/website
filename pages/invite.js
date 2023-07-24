3e55e129a4ee386eddb96c45888ece0b20e15974
Nox <patrik.tuominen@outlook.com>
2023-07-24 13:56:53 +0300

    ✨ feat: Bot invite link redirect page

---
+++ pages/invite.js
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+const Invite = () => {
+    const router = useRouter();
+
+    useEffect(() => {
+        if(typeof window !== 'undefined'){
+            router.push('https://discord.com/api/oauth2/authorize?client_id=1124332053027102761&permissions=8&scope=bot%20applications.commands')
+        }
+    }, [])
+
+    return null;
+}
+
+export default Invite;
